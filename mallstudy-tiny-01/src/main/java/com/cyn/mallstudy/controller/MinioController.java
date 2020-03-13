dd66f103891ebd684a5706274324293afb48bdd3
chenyn22577 <chenyn22577@hundsun.com>
2020-03-13 18:27:27 +0800

    add:增加md5

--- mallstudy-tiny-01/src/main/java/com/cyn/mallstudy/controller/MinioController.java
+++ mallstudy-tiny-01/src/main/java/com/cyn/mallstudy/controller/MinioController.java
@@ -1,5 +1,6 @@
 package com.cyn.mallstudy.controller;
 
+import cn.hutool.crypto.digest.MD5;
 import com.cyn.mallstudy.common.api.CommonResult;
 import com.cyn.mallstudy.dto.MinioUploadDto;
 import io.minio.MinioClient;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * 文件描述
@@ -59,16 +62,21 @@ public class MinioController {
                 minioClient.makeBucket(BUCKET_NAME);
                 minioClient.setBucketPolicy(BUCKET_NAME, "*.*", PolicyType.READ_ONLY);
             }
+            InputStream in =file.getInputStream();
             String filename = file.getOriginalFilename();
+            MD5 md5 = new MD5();
+            String md5Str =  md5.digestHex(in);
+            String fileExt = filename.substring(filename.lastIndexOf("."));
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
             // 设置存储对象名称
-            String objectName = sdf.format(new Date()) + "/" + filename;
+            String objectName = sdf.format(new Date()) + "/" + UUID.randomUUID() +"."+fileExt;
             // 使用putObject上传一个文件到存储桶中
             minioClient.putObject(BUCKET_NAME, objectName, file.getInputStream(), file.getContentType());
             LOGGER.info("文件上传成功!");
             MinioUploadDto minioUploadDto = new MinioUploadDto();
-            minioUploadDto.setName(filename);
+            minioUploadDto.setName(objectName);
             minioUploadDto.setUrl(ENDPOINT + "/" + BUCKET_NAME + "/" + objectName);
+            LOGGER.info("文件object-id: "+objectName);
             return CommonResult.success(minioUploadDto);
         } catch (Exception e) {
             LOGGER.info("上传发生错误: {}！", e.getMessage());
@@ -86,6 +94,28 @@ public class MinioController {
             return CommonResult.success(null);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("文件下载")
+    @RequestMapping(value = "/download", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult download(@RequestParam("filename") String filename) {
+        try {
+            MinioClient minioClient = new MinioClient(ENDPOINT, ACCESS_KEY, SECRET_KEY);
+            InputStream stream = minioClient.getObject(BUCKET_NAME, filename);
+
+            // 读取输入流直到EOF并打印到控制台。
+            byte[] buf = new byte[16384];
+            int bytesRead;
+            while ((bytesRead = stream.read(buf, 0, buf.length)) >= 0) {
+                System.out.println(new String(buf, 0, bytesRead));
+            }
+            // 关闭流，此处为示例，流关闭最好放在finally块。
+            stream.close();
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e);
         }
         return CommonResult.failed();
     }
